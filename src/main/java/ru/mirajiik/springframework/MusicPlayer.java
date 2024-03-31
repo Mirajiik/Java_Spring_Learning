a47f2efcdbca2500e32461951ee196412ad0c79a
Miraj <mirajiik@yandex.ru>
2024-03-31 17:02:19 +0500

    Using context property placeholder

--- src/main/java/ru/mirajiik/springframework/MusicPlayer.java
+++ src/main/java/ru/mirajiik/springframework/MusicPlayer.java
@@ -2,10 +2,25 @@ package ru.mirajiik.springframework;
 
 public class MusicPlayer {
     private Music music;
-
-    // IoC
+    private String name;
+    private Integer volume;
+    // Constructors
     public MusicPlayer(Music music) {
         this.music = music;
+    }
+    public MusicPlayer(){ }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setVolume(Integer volume){
+        this.volume = volume;
+    }
+    public Integer getVolume() {
+        return volume;
     }
 
     public void playMusic() {
